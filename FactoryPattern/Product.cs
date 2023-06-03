90d4b202e69ac5f7c51cb8d68c1215d1db5f128c
Hasib Ahrari <hasibahrari@gmail.com>
2023-06-02 21:18:52 -0700

    Final Commit

---
+++ FactoryPattern/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryExercise2
+{
+    public class Product
+    {
+        
+
+        public int Price { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace FactoryPattern
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryExercise2
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            bool correctInput;
+            do {
+                Console.Clear();
+                correctInput = true;
+
+                Console.WriteLine("Hello, What databade would you like to work with!");
+                Console.WriteLine("Type: sql");
+                Console.WriteLine("Type: mongo");
+                Console.WriteLine("Type list");
+
+
+                userInput = Console.ReadLine();
+
+                if(userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Bad Input!");
+                    Thread.Sleep(1000);
+                }
+            } while (!correctInput); ;
+
+            Console.Clear();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData();
+            //db.SaveData();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
